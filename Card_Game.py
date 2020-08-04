dbb8690e8c74f6f474766d91a3a7b95b6fde33c5
ngahet <ngahet@stu.otc.school.nz>
2020-08-04 12:52:27 +1200

    This is an example of a commit.

--- Card_Game.py
+++ Card_Game.py
@@ -8,3 +8,7 @@ Twos = ["2.Clubs", "2.Spades", "2.Diamonds", "2.Hearts"]
 Threes = ["3.Clubs", "3.Spades", "3.Diamonds", "3.Hearts"]
 Fours = ["4.Clubs", "4.Spades", "4.Diamonds", "4.Hearts"]
 Fives = ["5.Clubs", "5.Spades", "5.Diamonds", "5.Hearts"]
+
+#For demo purposes here is a new line of code.
+print("Hello")
+
