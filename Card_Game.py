586b2f3c34c58ec10d1eddd3d4e0262bc56b50de
TevitaN <69176773+TevitaN@users.noreply.github.com>
2020-08-04 12:47:53 +1200

    Create Card_Game.py
    First Commit

---
+++ Card_Game.py
@@ -0,0 +1,10 @@
+#Imports:
+import time
+import random 
+
+#Variables - Cards and Suits
+Aces = ["AceClubs", "AceSpades", "AceDiamonds", "AceHearts"]
+Twos = ["2.Clubs", "2.Spades", "2.Diamonds", "2.Hearts"]
+Threes = ["3.Clubs", "3.Spades", "3.Diamonds", "3.Hearts"]
+Fours = ["4.Clubs", "4.Spades", "4.Diamonds", "4.Hearts"]
+Fives = ["5.Clubs", "5.Spades", "5.Diamonds", "5.Hearts"]
