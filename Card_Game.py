2c4dec928abf57e48f3eaa07384d356098f7f573
ngahet <ngahet@stu.otc.school.nz>
2020-08-04 13:20:15 +1200

    Finished Card and Suits List

--- Card_Game.py
+++ Card_Game.py
@@ -2,13 +2,18 @@
 import time
 import random 
 
-#Variables - Cards and Suits
+#Lists - Cards and Suits
 Aces = ["AceClubs", "AceSpades", "AceDiamonds", "AceHearts"]
 Twos = ["2.Clubs", "2.Spades", "2.Diamonds", "2.Hearts"]
 Threes = ["3.Clubs", "3.Spades", "3.Diamonds", "3.Hearts"]
 Fours = ["4.Clubs", "4.Spades", "4.Diamonds", "4.Hearts"]
 Fives = ["5.Clubs", "5.Spades", "5.Diamonds", "5.Hearts"]
-
-#For demo purposes here is a new line of code.
-print("Hello")
+Sixes = ["6.Clubs", "6.Spades", "6.Diamonds", "6.Hearts"]
+Sevens = ["7.Clubs", "7.Spades", "7.Diamonds","7.Hearts"]
+Eights = ["8.Clubs","8.Spades", "8.Diamonds", "8.Hearts"]
+Nines = ["9.Clubs", "9.Spades", "9.Diamonds", "9.Hearts"]
+Tens = ["10.Clubs", "10.Spades", "10.Diamonds", "10.Hearts"]
+Jacks = ["J.Clubs", "J.Spades", "J.Diamonds", "J.Hearts"]
+Queens = ["Q.Clubs", "Q.Spades", "Q.Diamonds", "Q.Hearts"]
+Kings = ["K.Clubs", "K.Spades", "K.Diamonds", "K.Hearts"]
 
